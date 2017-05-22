2852723c8deef8daef8c5d14e8eb79eedebb077a
joppich <lejay@physik.tu-berlin.de>
2017-05-22 20:07:18 +0200

    aws ec2 boilerplate

--- app/lib/aws.py
+++ app/lib/aws.py
@@ -1,7 +1,26 @@
-# methods for spawning and destroying ssh capable aws instances
+import boto3
+import os
 
-def deploy_aws_instance(publickey):
-	pass
+class AwsEc2(object):
+    # methods for spawning and destroying ssh capable aws instances
+    ami = os.environ.get('AWS_AMI')
 
-def destroy_aws_instance(ip):
-	pass
+    def deploy_aws_instance(self,publickey):
+        i = self.ec2.create_instances(
+            ImageId=self.ami,
+            MinCount=1,
+            MaxCount=2,
+            InstanceType='t2.micro'
+        )
+        i.start()
+
+    def destroy_aws_instance(self,ip):
+        for ins in self.ec2.instances.all():
+            if ins.public_ip_address == ip:
+                ins.terminate()
+
+    def __init__(self):
+        try:
+            self.ec2 = boto3.resource('ec2')
+        except:
+            raise()
